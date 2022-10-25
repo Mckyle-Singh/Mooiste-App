e28acf77ffcc76b3e725dd086ddf593d5cabbb2a
Denzel <email@denzel>
2022-10-25 22:00:07 +0200

    admin package

---
+++ app/src/main/java/vcpe/st10118615/mooiste/admin/AdminHome.java
@@ -0,0 +1,16 @@
+package vcpe.st10118615.mooiste.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vcpe.st10118615.mooiste.R;
+
+public class AdminHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+}
---
+++ app/src/main/java/vcpe/st10118615/mooiste/admin/NewProductActivity.java
@@ -0,0 +1,13 @@
+package vcpe.st10118615.mooiste.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
